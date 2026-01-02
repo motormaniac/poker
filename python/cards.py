555ded92e59790198f7451aaadfb74f4640206dc
motormaniac <darrienlee626@gmail.com>
2026-01-02 13:24:19 -0500

    refactor which code goes in which files

--- python/cards.py
+++ python/cards.py
@@ -1,4 +1,5 @@
 import sys
+import random
 sys.path.append(".")
 from python.constants import *
 from python.spritesheet import SpriteSheet
@@ -17,3 +18,23 @@ for col in range(13):
         image = card_spritesheet.get_image(col, row)
         CARD_IMGS.append(image)
 CARD_BACK_IMG = card_spritesheet.get_image(0, 4)
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __repr__(self):
+        return f"{self.rank}{self.suit}"
+
+class Deck:
+    suits = ["H", "D", "C", "S"] #hearts diamonds clubs spades
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    def __init__(self):
+        self.cards = []
+        for suit in self.suits:  #iterating through suits and ranks
+            for rank in self.ranks:
+                self.cards.append(Card(suit, rank)) #add new card to deck
+            random.shuffle(self.cards) #randomizing card order
+    def deal(self):
+            return self.cards.pop()   #takes last card out and returns it
--- python/evaluate_hand.py
+++ python/evaluate_hand.py
@@ -1,34 +1,13 @@
-import random
-
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-    def __repr__(self):
-        return f"{self.rank}{self.suit}"
-
-class Deck:
-    suits = ["H", "D", "C", "S"] #hearts diamonds clubs spades
-    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
-    def __init__(self):
-        self.cards = []
-        for suit in self.suits:  #iterating through suits and ranks
-            for rank in self.ranks:
-                self.cards.append(Card(suit, rank)) #add new card to deck
-            random.shuffle(self.cards) #randomizing card order
-    def deal(self):
-            return self.cards.pop()   #takes last card out and returns it                 
-
-
- 
+import sys
+sys.path.append(".")
+from python.cards import Deck
 
 def count_ranks(hand):
     counts = {}
     for card in hand:   
         rank = card.rank
         if rank in counts:
-            counts[rank] = counts[rank] + 1.  #counting how many cards of same rank are in deck
+            counts[rank] += 1.  #counting how many cards of same rank are in deck
         else:
             counts[rank] = 1
     return counts
